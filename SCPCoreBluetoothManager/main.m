5fc92368c137458e41e45648d20213b52f684d61
Ste Prescott <mail@ste.me>
2014-05-22 02:05:05 +0100

    App files including storyboards

---
+++ SCPCoreBluetoothManager/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SCPCoreBluetoothManager
+//
+//  Created by Ste Prescott on 02/05/2014.
+//  Copyright (c) 2014 Ste Prescott. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SCPAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([SCPAppDelegate class]));
+	}
+}
