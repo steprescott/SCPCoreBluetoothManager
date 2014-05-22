ad33d2145e8dfcbea452c6190589856a71094b0a
Ste Prescott <mail@ste.me>
2014-05-22 02:06:26 +0100

    SCPCoreBluetoothManager files.
    This only includes the central manager part. The Peripheral manager part is in development.

---
+++ SCPCoreBluetoothManager/SCPCoreBluetoothManager/SCPCoreBluetoothManager Categories/NSString+SCPCoreBluetoothManager.h
@@ -0,0 +1,15 @@
+//
+//  NSString+SCPCoreBluetoothManager.h
+//  SCPCoreBluetoothManager
+//
+//  Created by Ste Prescott on 06/05/2014.
+//  Copyright (c) 2014 Ste Prescott. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (SCPCoreBluetoothManager)
+
++ (NSString *)stringFromHexString:(NSString *)hexString;
+
+@end
